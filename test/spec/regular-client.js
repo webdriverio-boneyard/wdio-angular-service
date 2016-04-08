ad291696c1ada405f1c2681e6679bdb2bc69dfc3
Christian Leutloff <leutloff@sundancer.oche.de>
2016-04-09 01:03:50 +0200

    Reworked the test cases to work with WebdriverIO 4.0.5.

--- test/spec/regular-client.js
+++ test/spec/regular-client.js
@@ -1,27 +1,15 @@
-describe('a regular webdriverio client', function() {
-  var webdriverio = require('webdriverio');
-  var client;
+describe('a regular webdriverio client', function () {
+    var assert = require('assert');
 
-  before(function(done) {
-    client = webdriverio
-      .remote(webdriverjsOptions)
-      .init()
-      .url('http://localhost:8000/test/app/app/index.html#/view1', done);
-  });
+    before(function () {
+        browser.url('http://localhost:8000/test/app/app/index.html#/view1');
+    });
 
-  after(function(done) {
-    client.end(done);
-  });
+    it('does not wait for angularjs when clicking', function () {
+        browser.pause(2000);  // wait for page load
 
-  it('does not wait for angularjs when clicking', function(done) {
-    client
-      // wait for page load
-      .pause(2000)
-      .click('[href="#/view2"]')
-      .getText('.ng-scope:nth-child(1)', function(err, text) {
-        assert.equal(err, null);
+        browser.click('[href="#/view2"]');
+        var text = browser.getText('.ng-scope:nth-child(1)');
         assert.equal(text, 'This is the partial for view 1.');
-        done(err);
-      });
-  });
+    });
 });
